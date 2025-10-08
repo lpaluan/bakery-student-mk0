29afc5040b3a11c85d2855f242d940d90fffbe20
Leo P. <lpaluan@gmail.com>
2025-10-08 19:42:10 +0200

    Auto-generated commit

--- chunk-2VL433B2.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-I5LOKIP6.js";import"./chunk-YMT33ZUX.js";import"./chunk-PZ5UXJ7I.js";export{a as QrReader};
---
+++ chunk-MVFDMPQE.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-E222U24V.js";import"./chunk-YMT33ZUX.js";import"./chunk-PZ5UXJ7I.js";export{a as QrReader};
