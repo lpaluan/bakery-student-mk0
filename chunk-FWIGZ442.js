288d5c07a603b6f4a323d4f3f83f66042be7913b
Leo P. <lpaluan@gmail.com>
2025-10-08 10:39:05 +0200

    Auto-generated commit

--- chunk-FWIGZ442.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-NTIKI4FK.js";import"./chunk-PNTZTF4L.js";import"./chunk-FK42CRUA.js";export{a as QrReader};
---
+++ chunk-SB7C74YS.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FJUNIVZ2.js";import"./chunk-PNTZTF4L.js";import"./chunk-FK42CRUA.js";export{a as QrReader};
